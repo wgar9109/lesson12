cfcc6fa81eb3fa99c1f20f30b47d810402a7ca15
wgar9109 <tylergary09@gmail.com>
2023-04-01 11:34:54 -0500

    Corrected js file

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ const img = document.querySelector(".img");
 const getImage = async function () {
     const res = await fetch (" https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(data);
+    console.log(images);
 };
 getImage();
